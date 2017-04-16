6850bf961a7565e34128f5a9a68ebb5921bf244e
Pedro Mendonca <peumendonca@gmail.com>
2017-04-16 16:19:23 +0100

    Generating game and code

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var express = require("express"),
-	app = express()
-
+"use strict"
 const port = 3000
+var express = require("express"),
+	app     = express(),
+	io      = require('socket.io')(port+1)
+
+//Global varaibles
+var games = []
+var curGame = 0;
 
 app.set('view engine', 'pug')
 app.use(express.static('static'))
@@ -13,3 +18,50 @@ app.listen(port,()=> {
 app.get('/',(req,res)=>{
 	res.render('index')
 })
+app.get('/make',(req,res)=>{
+	var tmp = new Game()
+	games.push(tmp)
+	console.log("New game with code " + tmp.getCode() + " started")
+	console.log("Current games:" + games.length)
+	res.send(tmp.getCode())
+})
+
+//Logic stuff
+class Game {
+	constructor(){
+		this.code = generateCode()
+		this.players = []
+		this.round = 1
+	}
+	getCode(){
+		return this.code;
+	}
+}
+
+class Player {
+	constructor(name) {
+		this.name = name
+		this.score = 0
+		this.plays = 0
+	}
+}
+//TODO: make it static 
+function generateCode()
+{
+	var code = [0,0,0,0]
+	code[3] = curGame%26
+	code[2] = (Math.floor(curGame/26))%26
+	code[1] = (Math.floor(curGame/(26*26)))%26
+	code[0] = (Math.floor(curGame/(26*26*26)))%26
+	curGame++;
+	var s = "";
+	code.forEach((letter)=>{
+		s += String.fromCharCode(65+letter)
+	})
+	return s
+	// return "AAAA";
+}
+//find a specific game
+function findGame(code) {
+	return games.filter((game)=>{return game.getCode == code})
+}
