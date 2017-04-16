0ecad34a0c10076112fddae7d2aa8f5812fcf658
Pedro Mendonca <peumendonca@gmail.com>
2017-04-16 15:05:39 +0100

    index

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require("express"),
-	  app = express()
+var express = require("express"),
+	app = express()
 
 const port = 3000
 
 app.set('view engine', 'pug')
+app.use(express.static('node_modules/startup.css/css'))
 app.listen(port,()=> {
 	console.log("Starting server on port "+ port)
 })
