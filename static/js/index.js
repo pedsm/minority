e2870340174f96f4c8a2aba7ada12ace07eb8afa
Pedro Mendonca <peumendonca@gmail.com>
2017-04-16 20:54:37 +0100

    Lots of waiting room stuff

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,7 @@ var makeBt = document.getElementById("button1")
 var joinBt = document.getElementById("button2")
 var container = document.getElementById("container")
 var playerlist = document.getElementById("players")
+var msg = document.getElementById("msg")
 
 var socket = io()
 var game   = null
@@ -15,37 +16,59 @@ var id = null
 makeBt.onclick = makeGame
 joinBt.onclick = chooseName
 
+//Makes a game
 function makeGame() {
 	socket.emit('make', input.value)
 	gameMaster = true;
 	container.style.margin = "auto"
 	makeBt.childNodes[0].innerHTML = "Nobody is in..."
 	joinBt.style.display = "none"
+	msg.style.display = "block";
 }
+//Chooses the name for anyone joining
 function chooseName(){
 	input.disabled = true
 	input2.style.display = "block"
 	makeBt.style.display = "none"
 	joinBt.onclick = joinGame
 }
+//Joins an existing game after setting a name
 function joinGame(){
 	socket.emit('join', {name:input.value,room:input2.value})
+	input.style.display = "none"
+	input2.style.display = "none"
+	joinBt.childNodes[0].innerHTML = "Wait for the start"
+	joinBt.onclick = ()=>{}
 }
+//Updates the list as soon as someone joins
 function updateList() {
 	playerlist.innerHTML = ""
 	game.players.forEach((player)=>{
 		playerlist.innerHTML += "<li>" + player.name + "</li>"
 	})
 }
-
+//Starts the game
+function startGame() {
+	
+}
+//Update's the list of players on the waiting room
 socket.on('updateGame',(data)=>{
 	game = data
 	updateList()
+	if(game.players.length == 2) {
+		makeBt.childNodes[0].innerHTML = "You need more than 2"
+	}
+	if(game.players.length > 2) {
+		makeBt.childNodes[0].innerHTML = "Everybody's in!"
+		startGame()
+	}
 })
+//Receives and stores the game state
 socket.on('joined',(data)=> {
 	game = data.game
 	id = data.id
 	player = game.players[id]
 	console.log("Joined a room")
+	msg.innerHTML = "Your room code: " + game.code
 	updateList()
 })
